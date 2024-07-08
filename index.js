e89e595d72c75b4ab4aad87e90d42027e6386105
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2024-07-08 15:18:06 +0530

    changed to GET

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post("/webhook",(req,res)=>{ //i want some
 
             axios({
                 method:"POST",
-                url:"https://dstaevents.in/demo/app/test.php",
+                url:"https://dstaevents.in/demo/app/test.php?from="+from,
                 data:{
                     messaging_product:"whatsapp",
                     from:from,
