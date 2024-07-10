ebe868668a96d833ce0d13218f2d26bf621f11e2
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2024-07-10 16:41:02 +0530

    update message type

--- index.js
+++ index.js
@@ -46,7 +46,34 @@ app.post("/webhook",(req,res)=>{ //i want some
             ){
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+                let msg_type=body_param.entry[0].changes[0].value.messages[0].type;
+                
+                console.log(msg_type);
+
+                if(msg_type=='text'){
+
+                    console.log('text message');
+
+                }
+
+
+
+
+
+
+                if(msg_type=='interactive'){
+                    let user_reply=body_param.entry[0].changes[0].value.messages[0].interactive.list_reply.title;
+                    console.log(user_reply);
+                    msg_body=user_reply;
+                }
+
+               try {
+                msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+               } catch (error) {
+                msg_body ='blank';
+               }
+               
              let msg = body_param.entry[0].changes[0].value.messages[0];
 
                let msg_id =  body_param.entry[0].changes[0].value.messages[0].id; 
@@ -82,7 +109,7 @@ app.post("/webhook",(req,res)=>{ //i want some
 
             axios({
                 method:"POST",
-                url:"https://app.primlyapp.com/?productname=whatsapp&appname=fresh-menu&fromphone="+from+"&msg="+msg+"&fromphoneid="+phon_no_id+"&msgid="+msg_id,
+                url:"https://app.primlyapp.com/?productname=whatsapp&appname=fresh-menu&fromphone="+from+"&msg="+msg_body+"&fromphoneid="+phon_no_id+"&msgid="+msg_id,
                 data:{
                    from:from,
                 },
